499ca2c86cfddcca2ee46a6c95dd2da59d2488a2
Pedro Maranhão <89420808+joaopmarinho@users.noreply.github.com>
2023-01-29 14:31:20 -0300

    Update AluAndTest.sv

--- testbenches/ALU/AluAndTest.sv
+++ testbenches/ALU/AluAndTest.sv
@@ -1,4 +1,4 @@
-module AluAndTest;
+module AluSubAddTest;
     import cv32e40p_pkg::*;
 
     logic           [31:0]  result_o;
@@ -25,19 +25,48 @@ module AluAndTest;
     cv32e40p_alu dut(.*);
 
     initial begin
-        $monitor($time," - sel = %b: a = %b | b = %b | res = %b", operator_i, operand_a_i, operand_b_i, result_o);
+        $monitor($time," - sel = %b: a = %d | b = %d | res = (%d) %b", operator_i, operand_a_i, operand_b_i, result_o, result_o);
 
         // Valores iniciais
-        operator_i = ALU_AND;
+        enable_i = 1;
+        operand_c_i = 32'd0;
+        vector_mode_i = 2'd0;
+        bmask_a_i = 5'd0;
+        bmask_b_i = 5'd0;
+        imm_vec_ext_i = 2'd0;
+        is_clpx_i = 1;
+        is_subrot_i = 0;
+        clpx_shift_i= 2'd0;
+        ex_ready_i = 1;
+        // Quais dessas inputs influenciam no resultado ??
+        // teste com resultado negativo dando pau
+        operator_i = ALU_ADD;
         operand_a_i = 32'd0;
         operand_b_i = 32'd0;
 
-        // Após 10, 'b' muda para 1. 'a' continua 0 e 'operator_i' continua ALU_AND
+        // Após 10, 'b' muda para 10. 'a' muda para 1
         #10
-        operand_b_i = 32'd1;
-        
-        // Após 10, 'a' muda para 1. 'b' continua 1 e 'operator_i' continua ALU_AND
+        operand_b_i = 32'd30;
+        operand_a_i = 32'd10;
+
+        // Após 10, 'b' muda para 5. 'a' muda para 25
         #10
-        operand_a_i = 32'd1;
+        operand_b_i = 32'd5;
+        operand_a_i = 32'd25;
+
+        #10
+        operator_i = ALU_SUB;
+        operand_a_i = 32'd0;
+        operand_b_i = 32'd0;
+
+        // Após 10, 'b' muda para 10. 'a' muda para 1
+        #10
+        operand_b_i = 32'd10;
+        operand_a_i = 32'd1; // retorna o resultado em complemento de 2
+
+        // Após 10, 'b' muda para 5. 'a' muda para 25
+        #10
+        operand_b_i = 32'd5;
+        operand_a_i = 32'd25;
     end
-endmodule: AluAndTest
+endmodule: AluSubAddTest
